2da64e949289504dcf0e1c302c2642357669547c
Tyler Barabas <tylerbarabas@gmail.com>
2018-09-13 04:26:19 -0700

    back end should be good for now...

--- server/middlewares/fileSystem.js
+++ server/middlewares/fileSystem.js
@@ -5,11 +5,18 @@ const appDir = path.dirname(require.main.filename);
 exports.getFilesInPath = (req, res) => {
     const relpath = path.join(`${appDir}/data`, req.body.path);
     fs.readdir( relpath, (error, items)=>{
-      console.log('READ DIR', items);
-      res.send(JSON.stringify({
+      let dir = {
         files: [],
         directories: []
-      }));
-    });
+      };
 
+      for (let i=0;i<items.length;i+=1){
+        let item = items[i];
+        let isDirectory = fs.lstatSync(path.join(relpath, item)).isDirectory();
+        if (isDirectory) dir.directories.push(item)
+        else dir.files.push(item);
+      }
+
+      res.send(JSON.stringify(dir));
+    });
 };
